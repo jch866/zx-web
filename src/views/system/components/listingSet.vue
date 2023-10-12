b9bc71fae1df1776b510a0696dfd00dd900e1160
EDZ <@eigpay.hangaoqiang>
2023-10-12 13:40:34 +0800

    feat:列表设置按钮UI优化

--- src/views/system/components/listingSet.vue
+++ src/views/system/components/listingSet.vue
@@ -4,11 +4,13 @@
       <div class="div_line">
         <i class="el-icon-close close_new" @click="closePopHandler"></i>
       </div>
-      <el-tree :data="data" show-checkbox node-key="id" :default-expanded-keys="[2, 3]" :default-checked-keys="[5]"
-        :props="defaultProps" draggable @node-drag-start="handleDragStart" @node-drag-enter="handleDragEnter"
-        @node-drag-leave="handleDragLeave" @node-drag-over="handleDragOver" @node-drag-end="handleDragEnd"
-        @node-drop="handleDrop">
-      </el-tree>
+      <div style="margin:0 10px">
+        <el-tree :data="data" show-checkbox node-key="id" :default-expanded-keys="[2, 3]" :default-checked-keys="[5]"
+          :props="defaultProps" draggable @node-drag-start="handleDragStart" @node-drag-enter="handleDragEnter"
+          @node-drag-leave="handleDragLeave" @node-drag-over="handleDragOver" @node-drag-end="handleDragEnd"
+          @node-drop="handleDrop">
+        </el-tree>
+      </div>
       <el-button size="small" icon="el-icon-setting" slot="reference">列表设置</el-button>
     </el-popover>
   </div>
@@ -19,7 +21,7 @@ export default {
   components: {
 
   },
-  data () {
+  data() {
     return {
       visible: false,
       data: [{
@@ -61,29 +63,29 @@ export default {
       }
     }
   },
-  created () {
+  created() {
     console.log(this)
   },
   methods: {
-    handleDragStart (node, ev) {
+    handleDragStart(node, ev) {
       console.log('drag start', node)
     },
-    handleDragEnter (draggingNode, dropNode, ev) {
+    handleDragEnter(draggingNode, dropNode, ev) {
       console.log('tree drag enter: ', dropNode.label)
     },
-    handleDragLeave (draggingNode, dropNode, ev) {
+    handleDragLeave(draggingNode, dropNode, ev) {
       console.log('tree drag leave: ', dropNode.label)
     },
-    handleDragOver (draggingNode, dropNode, ev) {
+    handleDragOver(draggingNode, dropNode, ev) {
       console.log('tree drag over: ', dropNode.label)
     },
-    handleDragEnd (draggingNode, dropNode, dropType, ev) {
+    handleDragEnd(draggingNode, dropNode, dropType, ev) {
       console.log('tree drag end: ', dropNode && dropNode.label, dropType)
     },
-    handleChange (value) {
+    handleChange(value) {
       console.log(value)
     },
-    closePopHandler () {
+    closePopHandler() {
       this.$refs.popoverlisting.doClose()
     }
   }
@@ -92,6 +94,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+:deep {
+  .el-tree-node__content {
+    height: 32px;
+    line-height: 32px
+  }
+
+  .el-tree-node__content::before {
+    content: '';
+    width: 5px;
+    height: 10px;
+    border-left: 2px dotted #606266;
+    border-right: 2px dotted #606266;
+    position: absolute;
+    margin-left: -6px;
+  }
+
+}
+
 .div_line {
   border-top: 1px solid #dddddd;
   margin: 0;
